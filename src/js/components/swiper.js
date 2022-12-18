52096b97f85c7faedf1c708466f32ddf033d197a
Gregtstu <boltnev.grisha@yandex.ru>
2022-12-18 11:42:40 +0300

    init

---
+++ src/js/components/swiper.js
@@ -0,0 +1,13 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
